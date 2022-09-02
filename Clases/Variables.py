74d2e5e61b311731dfeefdd0e8818f06fd61ca6c
Matiasgerb <mgerbaldo3@gmail.com>
2022-09-02 13:54:03 -0300

    Create Variables.py

---
+++ Clases/Variables.py
@@ -0,0 +1,19 @@
+#Variables
+from tokenize import Double
+
+
+mensaje = "Matias";
+print("Hola, me llamo: "+mensaje)
+mensajeInput =input("Ingrese un nombre: ")
+print("Hola, " +mensajeInput + " bienvenido")
+
+a= float(input('Ingrese un número: '))
+b= float(input('Ingrese otro número: '))
+resultSUM= a+b;
+resultRest= a-b;
+resultMulti = a*b;
+resultDivi =a/b;
+print("El resultado de la suma es: ", resultSUM)
+print("El resultado de la resta es: ", resultRest)
+print("El resultado de la multi es: ", resultMulti)
+print("El resultado de la division es: ",resultDivi)
