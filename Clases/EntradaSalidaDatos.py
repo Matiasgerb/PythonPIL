10dbcaa58631a55cf8161c2e42b2df89128c6de1
Matiasgerb <mgerbaldo3@gmail.com>
2022-09-02 13:37:02 -0300

    Clase1

---
+++ Clases/EntradaSalidaDatos.py
@@ -0,0 +1,23 @@
+#Salida de datos
+
+#Uso de print
+print('Hello World');
+
+#Otras funciones del print
+print(2+3);
+print(2-3);
+print(2*3);
+print(2/3);
+#Salto de linea
+print("Esto es un salto  \n de linea ");
+#Con esto se tabula
+print("Con esto se \t tabula");
+#Concatenar
+print("Esto es " + "una concatenación");
+#SEP
+
+print("Esto es ",  "una concatenación", sep='+');
+
+#Entrada de datos
+print("Ingrese un número: ")
+input()
