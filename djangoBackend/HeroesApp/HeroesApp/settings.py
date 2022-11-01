ef17836e6ab048c9b6266d91dfec256f2a47fc73
Matiasgerb <mgerbaldo3@gmail.com>
2022-11-01 13:40:58 -0300

    Update settings.py

--- djangoBackend/HeroesApp/HeroesApp/settings.py
+++ djangoBackend/HeroesApp/HeroesApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bhujzky&4=olvx4-x=2n7gn)&qu9ki8=efvu!edr90+()40&(f'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
